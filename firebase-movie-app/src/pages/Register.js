7414d3217b777392140bb2fba70e17a0972af012
tceyhan <55tc155@gmail.com>
2022-04-11 22:41:39 +0300

    register js added

--- firebase-movie-app/src/pages/Register.js
+++ firebase-movie-app/src/pages/Register.js
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Register = () => {
-  return (
-    <div className='d'>
-      
-    </div>
-  )
-}
 
-export default Register
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="form-image">
+        <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
+      </div>
+      <div className="register-form">
+        <h1 className="form-title display-3">Register</h1>
+        <form id="register">
+          <div className="mb-3">
+            <label htmlFor="first-name" className="form-label">
+              First Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="first-name"
+              placeholder="Enter your first name.."
+              onChange={(e) => setFirstname(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="last-name" className="form-label">
+              Last Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="last-name"
+              placeholder="Enter your last name.."
+              onChange={(e) => setLastname(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter your email adress.."
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter your password.."
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
